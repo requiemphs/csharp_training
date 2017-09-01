a95db45981dc6b52949ef2bdd02bbe1df1e84d52
KONTUR\sokolovmv <sokolovmv@skbkontur.ru>
2017-09-01 11:37:56 +0500

    Проект создан

---
+++ Addressbook-web-tests/Addressbook-web-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Addressbook-web-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SKB Kontur")]
+[assembly: AssemblyProduct("Addressbook-web-tests")]
+[assembly: AssemblyCopyright("Copyright © SKB Kontur 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98176fe7-fbcf-4742-8509-4c79687f55ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
