190427bd59aa80298deb96cb1990c4cae22177db
KONTUR\sokolovmv <sokolovmv@skbkontur.ru>
2017-10-07 11:52:53 +0500

    Генератор тестовых данных для групп в форматах csv, xml, json

--- Addressbook-web-tests/Addressbook-web-tests/Tests/GroupCreationTests.cs
+++ Addressbook-web-tests/Addressbook-web-tests/Tests/GroupCreationTests.cs
@@ -4,7 +4,11 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using NUnit.Framework;
+
 
 
 namespace WebAddressbookTests
@@ -26,7 +30,7 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        public static IEnumerable<GroupData> GroupDataFromFile()
+        public static IEnumerable<GroupData> GroupDataFromVCsvFile()
         {
             List<GroupData> groups = new List<GroupData>();
             string[] lines = File.ReadAllLines(@"groups.csv");
@@ -42,7 +46,21 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        [Test, TestCaseSource("GroupDataFromFile")]
+        public static IEnumerable<GroupData> GroupDataFromXmlFile()
+        {
+
+            return (List<GroupData>)
+                new XmlSerializer(typeof(List<GroupData>))
+                .Deserialize(new StreamReader(@"groups.xml"));
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromJsonFile()
+        {
+           return  JsonConvert.DeserializeObject<List<GroupData>>(
+                File.ReadAllText(@"groups.json"));
+        }
+
+        [Test, TestCaseSource("GroupDataFromJsonFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = applicationManager.Group.GetGroupList();
--- Addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ Addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using WebAddressbookTests;
+using Formatting = Newtonsoft.Json.Formatting;
 
-namespace WebAddressbookTests
+namespace addressbook_test_data_generators
 {
     class Program
     {
@@ -14,13 +18,54 @@ namespace WebAddressbookTests
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[2];
+
+            List<GroupData> groups = new List<GroupData>(); 
             for (int i = 0; i < count; i++)
             {
-                writer.WriteLine(String.Format("${0},${1},${2}",
-                    TestBase.GenerateRandomString(10),
-                    TestBase.GenerateRandomString(10),
-                    TestBase.GenerateRandomString(10)));
+                groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                {
+                    Header = TestBase.GenerateRandomString(10),
+                    Footer = TestBase.GenerateRandomString(10)
+                });
+
             }
+            if (format == "csv")
+            {
+                WriteGroupsToCsvFile(groups, writer);
+            }
+            else if (format == "xml")
+            {
+                WriteGroupsToJsonFile(groups, writer);
+            }
+            else if (format == "json")
+            {
+                WriteGroupsToJsonFile(groups, writer);
+            }
+            else
+            {
+                System.Console.Out.Write("Unrecognized format" + format);
+            }
+            writer.Close();
+        }
+
+        static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach (GroupData group in groups)
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",
+                    group.Name, group.Header, group.Footer));
+            }
+        }
+
+        static void WriteGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
+        }
+
+        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented ));
         }
     }
 }
